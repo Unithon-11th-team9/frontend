20203445b819f70e939d82ed83315950263ab78d
chldmswnl <chldmswnl12@gmail.com>
2024-10-01 22:13:12 +0900

    chore: reopen

--- src/pages/JoinPage/index.tsx
+++ src/pages/JoinPage/index.tsx
@@ -1,10 +1,10 @@
-// import Button from "@/components/Button";
+import Button from "@/components/Button";
 import Layout from "@/components/Layout";
 import Typography from "@/components/Typography";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function JoinPage() {
-	// const navigate = useNavigate();
+	const navigate = useNavigate();
 	return (
 		<Layout
 			classNames="relative"
@@ -16,21 +16,21 @@ export default function JoinPage() {
 					2024 대화 평화상
 				</Typography>
 				<img src="/main.png" className="h-72 w-72 pt-3" alt="main image" />
-				{/* <Button buttonType={"button1"} onClick={() => navigate("/upload")}>
+				<Button buttonType={"button1"} onClick={() => navigate("/upload")}>
 					우리 톡방 참가하기
-				</Button> */}
+				</Button>
 			</section>
 			<section className="w-full pb-6 pt-6 text-center">
-				<Typography classNames="whitespace-pre-wrap text-white" type={"main_text_03"}>
+				{/* <Typography classNames="whitespace-pre-wrap text-white" type={"main_text_03"}>
 					{`더 나은 서비스를 위해\n현재 점검 작업을 진행 중입니다.\n\n더 좋은 모습으로 돌아오겠습니다!`}
-				</Typography>
-				{/* <Typography type="sub_text_01">{`실제 카톡 대화를 보고 `}</Typography>
+				</Typography> */}
+				<Typography type="sub_text_01">{`실제 카톡 대화를 보고 `}</Typography>
 				<Typography type="sub_text_01" classNames="whitespace-pre-wrap text-point">
 					{`인공지능(AI)이 심사`}
 				</Typography>
 				<Typography type="sub_text_01" classNames="whitespace-pre-wrap">
 					{`해드립니다.\n대화 내용은 사람을 거치지 않고, 분석 후 즉시 자동 폐기됩니다.\n서비스 제공을 위한 용도 이외로 사용되지 않습니다.`}
-				</Typography> */}
+				</Typography>
 			</section>
 		</Layout>
 	);
